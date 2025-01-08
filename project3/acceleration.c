d0e6f7facf12a1135d37ce8a6b2b3b351aba627a
DimitraFyl <phylaktopoulou@gmail.com>
2025-01-08 12:55:35 +0100

    Update acceleration.c

--- project3/acceleration.c
+++ project3/acceleration.c
@@ -10,34 +10,39 @@ void compute_acc(
 
 
          //printf("This function calculate the acceleration.\n");	
-
+        // Iterate over each atom to calculate its acceleration
 	for (int atom = 0; atom < Natoms; atom++) {             // i 
-
+                // Loop over the three components (x, y, z) of acceleration
 		for (int comp = 0; comp < 3; comp++) {        // acceleration comp x, y,  and z
-
+                        // Initialize variables
 			double sigma_ovr6 = 0;
 			double sigma_ovr12 = 0;
 			double ur = 0;
 			double sum = 0;     // Sum of Ur * x-x/r
-
+                        // Loop over all the other atoms to calculate the force 
 			for (int anoth_atom = 0; anoth_atom < Natoms; anoth_atom++) {
-				if (atom != anoth_atom) {                                   // non diagonal atom value     // means if i != j
 
+				// skip the current atom (do not calculate force on itself)
+				if (atom != anoth_atom) {                                   // non diagonal atom value     // means if i != j
+                                        // compute the components of the Lennard- Jones potential
 					sigma_ovr6 = pow(sigma / distance[atom][anoth_atom], 6);
 					sigma_ovr12 = pow(sigma / distance[atom][anoth_atom], 12);
 
 					//printf("The distance value is: %f\n", distance[atom][anoth_atom]);
 
-
+                                        // Compute the potential derivative (force) between the two atoms
 					ur = 24 * (epsilon / distance[atom][anoth_atom]) * (sigma_ovr6 - 2 * sigma_ovr12);
 					//printf("The ur value is: %f\n",ur);
 
+					// Add the contribution to the sum 
 					sum += ur * ((coord[atom][comp] - coord[anoth_atom][comp]) / distance[atom][anoth_atom]);
 					//printf("The sum value is: %f\n", sum);
 				
 					
 				}
 			}
+
+			// Compute the final acceleration for this atom
 			acceleration[atom][comp] = (- sum / mass[atom]);
 
 			//printf("[%d][%d]", atom, comp);
